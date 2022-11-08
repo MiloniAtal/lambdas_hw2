70d670dfba454d5cc6c26817f1e433e02d71c122
Miloni Atal <miloniatal@gmail.com>
2022-11-08 01:06:04 -0500

    Changed lambda Two to current version

--- src/lambdaTwo.py
+++ src/lambdaTwo.py
@@ -8,7 +8,6 @@ import os
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-client = boto3.client('lexv2-runtime')
 def connecting_to_opensearch():
     region = 'us-east-1'
     service = 'es'
@@ -41,13 +40,16 @@ def search_intent(intent_request):
     tag1 = get_slots(intent)["tag1"]
     tag2 = get_slots(intent)["tag2"]
     logger.debug('{Here in search_intent}')
-    
+    q1 = ""
+    q2 = ""
     if(tag1):
         q1 = inter(tag1)
         logger.debug('{}'.format(q1))
     if(tag2):
         q2 = inter(tag2)
         logger.debug('{}'.format(q2))
+
+    '''        
     if(tag1 and tag2):
         query = str(tag1) + " or " + str(tag2)
 
@@ -56,16 +58,25 @@ def search_intent(intent_request):
 
     else:
         query = None
+    '''
+    
+    query = []
+    if len(q1):
+        query.append(q1)
+    if len(q2):
+        query.append(q2)
 
     logger.debug(query)
-    if(query):
-        return opensearch(query)
-    return []
-        
-    
-    
-
-""" --- Intents --- """
+    matching = []
+    for label in query:
+        es_response = opensearch_updated(label)
+        for res in es_response:
+            #https://b2-hw2-my2727-ma4338.s3.amazonaws.com/
+            matching.append('https://' + res['_source']['bucket'] + '.s3.amazonaws.com/' + res['_source']['objectKey'] )
+            #matching.append('s3://' + res['_source']['bucket'] + '/' +res['_source']['objectKey'])
+    logger.debug(matching)
+    logger.debug(list(set(matching)))
+    return list(set(matching)), query
 
 
 def dispatch(intent_request):
@@ -81,6 +92,22 @@ def dispatch(intent_request):
         return search_intent(intent_request)
 
     return []
+
+def opensearch_updated(value):
+    query = {
+        "query": {
+            "match": {
+                    "labels" : value
+            }
+        }
+    }
+    headers = { "Content-Type": "application/json" }
+    es_response_ = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
+    logger.debug('Here')
+    logger.debug(es_response_.text)
+    es_response = json.loads(es_response_.text).get("hits").get("hits")
+    return es_response
+    
 
 def opensearch(value):
     query = {
@@ -101,10 +128,9 @@ def opensearch(value):
     
     matching = []
     for res in es_response:
-        matching.append(res['_source']['objectKey'])
-    print(es_response_)
-    print(es_response_.text)
-    print(es_response)
+        objKey = res['_source']['objectKey']
+        if objKey not in matching:
+            matching.append('s3://' + res['_source']['bucket'] + '/' + objKey )
     return matching
 
 def all():
@@ -119,35 +145,52 @@ def all():
     print(es_response_)
     print(es_response_.text)
     return es_response_
-
-def create_index():    
-    headers = { "Content-Type": "application/json" }
-    host, index, url, awsauth = connecting_to_opensearch()
-    create = requests.put(host + '/' + index, auth=awsauth, json={}, headers=headers)
-    logger.debug(create.text)
-    r = requests.get(host + '/_cat/indices/', auth=awsauth, headers=headers)
-    logger.debug(r.text)
-
-
-
-
-def lambda_handler(event, context):
-    #TODO connect to frontent and get messages from there
-     
-    # msg_from_user = event['messages'][0]['unstructured']['text']
-    # sessionId = event['messages'][1]['unstructured']['text']
-    # disallowed_character = " ()-"
-    # # sessionId = "Tue Oct 11 2022 18:47:03 GMT-0400 (Eastern Daylight Time)"
-    # for character in disallowed_character:
-    #         sessionId = sessionId.replace(character,"")
-    # sessionId.replace("(", "")
-    # sessionId.replace(")", "")
-    # print(sessionId)
-    # print(event)
-    # print(context)
-    # print(f"Message from frontend: {msg_from_user}")
+# def old_lambda_handler(event, context):
+#     # tester()
+#     # opensearch("Person")
     
-    msg_from_user = "Show me dogs or person"
+#     # headers = { "Content-Type": "application/json" }
+#     # create = requests.put(host + '/' + index, auth=awsauth, json={}, headers=headers)
+#     # logger.debug(create.text)
+#     # r = requests.get(host + '/_cat/indices/', auth=awsauth, headers=headers)
+#     # logger.debug(r.text)
+#     # es_response_ = all()
+#     # logger.debug(es_response_.text)
+#     # es_response = json.loads(es_response_.text).get("hits").get("hits")
+
+#     dispatch(event)
+#     # return {
+#     #     'statusCode': 200,
+#     #     'body': json.dumps('Hello from Lambda!')
+#     # }
+#     return 0
+
+
+
+client = boto3.client('lexv2-runtime')
+host, index, url, awsauth = connecting_to_opensearch()
+def lambda_handler(event, context):
+    #Added message from 
+    #print(event) 
+    msg_from_user = event['queryStringParameters']['q']
+    #msg_from_user = event['messages'][0]['unstructured']['text']
+    #sessionId = event['messages'][1]['unstructured']['text']
+    
+    #disallowed_character = " ()-"
+    # sessionId = "Tue Oct 11 2022 18:47:03 GMT-0400 (Eastern Daylight Time)"
+    #for character in disallowed_character:
+    #        sessionId = sessionId.replace(character,"")
+    #sessionId.replace("(", "")
+    #sessionId.replace(")", "")
+    
+    
+    
+    #print(sessionId)
+    #print(event)
+    #print(context)
+    print(f"Message from frontend: {msg_from_user}")
+    
+    #msg_from_user = "Show me dogs or person"
     sessionId = "testuser"
     
     # Initiate conversation with Lex
@@ -160,6 +203,23 @@ def lambda_handler(event, context):
     
     # response = {"interpretations":[{"intent":{"confirmationState":"None","name":"SearchIntent","slots":{"tag1":{"value":{"interpretedValue":"person","originalValue":"person","resolvedValues":["person"]}},"tag2":None},"state":"ReadyForFulfillment"},"nluConfidence":{"score":1.0}},{"intent":{"name":"FallbackIntent","slots":{}}}],"requestAttributes":{},"sessionId":"testuser","sessionState":{"dialogAction":{"type":"Close"},"intent":{"confirmationState":"None","name":"SearchIntent","slots":{"tag1":{"value":{"interpretedValue":"person","originalValue":"person","resolvedValues":["person"]}},"tag2":None},"state":"ReadyForFulfillment"},"originatingRequestId":"e382a7f9-06dd-4265-a5a5-3e38c1baee92","sessionAttributes":{}}}
 
-    matched = dispatch(response)
-    return matched
-    
+    matched, labels = dispatch(response)
+    if len(matched):
+        
+        return{
+            'isBase64Encoded': False,
+            'statusCode': 200,
+            'headers': {"Access-Control-Allow-Origin":"*"},
+            'body': json.dumps({
+                'imagePaths':matched,
+                'userQuery':msg_from_user,
+                'labels': labels,
+            })
+        }
+    else:    
+        return{
+            'statusCode':200,
+            "headers": {"Access-Control-Allow-Origin":"*"},
+            'body': json.dumps('No Results found')
+        }
+        
